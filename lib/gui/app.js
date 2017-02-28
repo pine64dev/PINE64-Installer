dengcy028/etcher

feat(GUI): improve analytics events
This commit adds more events to our current analytics.
Will further improve in a future commit.

Change-Type: patch

See: https://github.com/resin-io/etcher/issues/1100

lib/gui/app.js
@@ -26,6 +26,7 @@ var angular = require('angular');
 const electron = require('electron');
 const EXIT_CODES = require('../shared/exit-codes');
 const messages = require('../shared/messages');
+const packageJSON = require('../../package.json');
 
 /* eslint-enable no-var */
 
@@ -85,22 +86,41 @@ app.run(() => {
 app.run((AnalyticsService, ErrorService, UpdateNotifierService, SelectionStateModel) => {
   AnalyticsService.logEvent('Application start');
 
-  if (UpdateNotifierService.shouldCheckForUpdates() && !process.env.ETCHER_DISABLE_UPDATES) {
-    AnalyticsService.logEvent('Checking for updates');
+  const shouldCheckForUpdates = UpdateNotifierService.shouldCheckForUpdates();
+
+  if (shouldCheckForUpdates && !process.env.ETCHER_DISABLE_UPDATES) {
+
+    AnalyticsService.logEvent('Checking for updates', {
+      currentVersion: packageJSON.version
+    });
 
     UpdateNotifierService.isLatestVersion().then((isLatestVersion) => {
+
+      if (isLatestVersion) {
+        AnalyticsService.logEvent('Running latest version');
+        return;
+      }
 
       // In case the internet connection is not good and checking the
       // latest published version takes too long, only show notify
       // the user about the new version if he didn't start the flash
       // process (e.g: selected an image), otherwise such interruption
       // might be annoying.
-      if (!isLatestVersion && !SelectionStateModel.hasImage()) {
-
-        AnalyticsService.logEvent('Notifying update');
+      if (!SelectionStateModel.hasImage()) {
         return UpdateNotifierService.notify();
       }
+
+      AnalyticsService.logEvent('Not notifying update because image already selected');
+
     }).catch(ErrorService.reportException);
+
+  } else {
+
+    AnalyticsService.logEvent('Not checking for updates', {
+      shouldCheckForUpdates,
+      ETCHER_DISABLE_UPDATES: process.env.ETCHER_DISABLE_UPDATES
+    });
+
   }
 
 });
@@ -134,7 +154,7 @@ app.run(($timeout, DriveScannerService, DrivesModel, ErrorService, DriveSelector
   DriveScannerService.on('drives', (drives) => {
 
     // Safely trigger a digest cycle.
-    // In some cases, AngularJS doesn't aknowledge that the
+    // In some cases, AngularJS doesn't acknowledge that the
     // available drives list has changed, and incorrectly
     // keeps asking the user to "Connect a drive".
     $timeout(() => {
@@ -150,11 +170,14 @@ app.run(($timeout, DriveScannerService, DrivesModel, ErrorService, DriveSelector
   DriveScannerService.start();
 });
 
-app.run(($window, WarningModalService, ErrorService, FlashStateModel, OSDialogService) => {
+app.run(($window, AnalyticsService, WarningModalService, ErrorService, FlashStateModel, OSDialogService) => {
   let popupExists = false;
 
   $window.addEventListener('beforeunload', (event) => {
     if (!FlashStateModel.isFlashing() || popupExists) {
+      AnalyticsService.logEvent('Close application', {
+        isFlashing: FlashStateModel.isFlashing()
+      });
       return;
     }
 
@@ -164,6 +187,8 @@ app.run(($window, WarningModalService, ErrorService, FlashStateModel, OSDialogSe
     // Don't open any more popups
     popupExists = true;
 
+    AnalyticsService.logEvent('Close attempt while flashing');
+
     return OSDialogService.showWarning({
       confirmationLabel: 'Yes, quit',
       rejectionLabel: 'Cancel',
@@ -171,6 +196,7 @@ app.run(($window, WarningModalService, ErrorService, FlashStateModel, OSDialogSe
       description: messages.warning.exitWhileFlashing()
     }).then((confirmed) => {
       if (confirmed) {
+        AnalyticsService.logEvent('Close confirmed while flashing');
 
         // This circumvents the 'beforeunload' event unlike
         // electron.remote.app.quit() which does not.
@@ -178,8 +204,18 @@ app.run(($window, WarningModalService, ErrorService, FlashStateModel, OSDialogSe
 
       }
 
+      AnalyticsService.logEvent('Close rejected while flashing');
       popupExists = false;
     }).catch(ErrorService.reportException);
+  });
+});
+
+app.run(($rootScope, AnalyticsService) => {
+  $rootScope.$on('$stateChangeSuccess', (evt, toState, toParams, fromState) => {
+    AnalyticsService.logEvent('Navigate', {
+      to: toState.name,
+      from: fromState.name
+    });
   });
 });
 

pine64dev/PINE64-Installer

lib/gui/app.js
@@ -26,6 +26,7 @@ var angular = require('angular');
 const electron = require('electron');
 const EXIT_CODES = require('../shared/exit-codes');
 const messages = require('../shared/messages');
+const packageJSON = require('../../package.json');
 
 /* eslint-enable no-var */
 
@@ -85,23 +86,44 @@ app.run(() => {
 app.run((AnalyticsService, ErrorService, UpdateNotifierService, SelectionStateModel) => {
   AnalyticsService.logEvent('Application start');
 
-  if (UpdateNotifierService.shouldCheckForUpdates() && !process.env.ETCHER_DISABLE_UPDATES) {
-    AnalyticsService.logEvent('Checking for updates');
+  const shouldCheckForUpdates = UpdateNotifierService.shouldCheckForUpdates();
 
-    UpdateNotifierService.isLatestVersion().then((isLatestVersion) => {
-
-      // In case the internet connection is not good and checking the
-      // latest published version takes too long, only show notify
-      // the user about the new version if he didn't start the flash
-      // process (e.g: selected an image), otherwise such interruption
-      // might be annoying.
-      if (!isLatestVersion && !SelectionStateModel.hasImage()) {
-
-        AnalyticsService.logEvent('Notifying update');
-        return UpdateNotifierService.notify();
-      }
-    }).catch(ErrorService.reportException);
+  if (!shouldCheckForUpdates || process.env.ETCHER_DISABLE_UPDATES) {
+    AnalyticsService.logEvent('Not checking for updates', {
+      shouldCheckForUpdates,
+      disableUpdatesEnvironmentVariable: process.env.ETCHER_DISABLE_UPDATES
+    });
+    return;
   }
+
+  AnalyticsService.logEvent('Checking for updates', {
+    currentVersion: packageJSON.version
+  });
+
+  UpdateNotifierService.isLatestVersion().then((isLatestVersion) => {
+
+    if (isLatestVersion) {
+      AnalyticsService.logEvent('Update notification skipped', {
+        reason: 'Latest version'
+      });
+      return;
+    }
+
+    // In case the internet connection is not good and checking the
+    // latest published version takes too long, only show notify
+    // the user about the new version if he didn't start the flash
+    // process (e.g: selected an image), otherwise such interruption
+    // might be annoying.
+    if (SelectionStateModel.hasImage()) {
+      AnalyticsService.logEvent('Update notification skipped', {
+        reason: 'Image selected'
+      });
+      return;
+    }
+
+    return UpdateNotifierService.notify();
+
+  }).catch(ErrorService.reportException);
 
 });
 
@@ -150,11 +172,14 @@ app.run(($timeout, DriveScannerService, DrivesModel, ErrorService, DriveSelector
   DriveScannerService.start();
 });
 
-app.run(($window, WarningModalService, ErrorService, FlashStateModel, OSDialogService) => {
+app.run(($window, AnalyticsService, WarningModalService, ErrorService, FlashStateModel, OSDialogService) => {
   let popupExists = false;
 
   $window.addEventListener('beforeunload', (event) => {
     if (!FlashStateModel.isFlashing() || popupExists) {
+      AnalyticsService.logEvent('Close application', {
+        isFlashing: FlashStateModel.isFlashing()
+      });
       return;
     }
 
@@ -164,6 +189,8 @@ app.run(($window, WarningModalService, ErrorService, FlashStateModel, OSDialogSe
     // Don't open any more popups
     popupExists = true;
 
+    AnalyticsService.logEvent('Close attempt while flashing');
+
     return OSDialogService.showWarning({
       confirmationLabel: 'Yes, quit',
       rejectionLabel: 'Cancel',
@@ -171,6 +198,7 @@ app.run(($window, WarningModalService, ErrorService, FlashStateModel, OSDialogSe
       description: messages.warning.exitWhileFlashing()
     }).then((confirmed) => {
       if (confirmed) {
+        AnalyticsService.logEvent('Close confirmed while flashing');
 
         // This circumvents the 'beforeunload' event unlike
         // electron.remote.app.quit() which does not.
@@ -178,8 +206,24 @@ app.run(($window, WarningModalService, ErrorService, FlashStateModel, OSDialogSe
 
       }
 
+      AnalyticsService.logEvent('Close rejected while flashing');
       popupExists = false;
     }).catch(ErrorService.reportException);
+  });
+});
+
+app.run(($rootScope, AnalyticsService) => {
+  $rootScope.$on('$stateChangeSuccess', (event, toState, toParams, fromState) => {
+
+    // Ignore first navigation
+    if (!fromState.name) {
+      return;
+    }
+
+    AnalyticsService.logEvent('Navigate', {
+      to: toState.name,
+      from: fromState.name
+    });
   });
 });
 